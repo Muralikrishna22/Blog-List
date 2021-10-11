937a2369802af0e907d72df7b72d0e99146a7e4d
Muralikrishna22 <yernenamuralikrishna@gmail.com>
2021-10-11 08:04:07 +0000

    Blog List

---
+++ src/components/BlogList/index.js
@@ -0,0 +1,16 @@
+import BlogItem from '../BlogItem'
+
+import './index.css'
+
+const BlogList = props => {
+  const {blogsList} = props
+  return (
+    <ul className="blogs-container">
+      {blogsList.map(eachBlog => (
+        <BlogItem blogDetails={eachBlog} key={eachBlog.id} />
+      ))}
+    </ul>
+  )
+}
+
+export default BlogList
